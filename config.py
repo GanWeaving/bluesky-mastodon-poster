d770ed2d59df6450c25912bb71c84c724be4d96c
GanWeaving <5080144+GanWeaving@users.noreply.github.com>
2023-05-10 15:37:01 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Bluesky and Mastodon API credentials
+BLUESKY_EMAIL = 'your_email'
+BLUESKY_PASSWORD = 'your_password'
+MASTODON_CLIENT_ID = 'your_client_id'
+MASTODON_CLIENT_SECRET = 'your_client_secret'
+MASTODON_ACCESS_TOKEN = 'your_access_token'
+MASTODON_API_BASE_URL = 'your_base_url'
